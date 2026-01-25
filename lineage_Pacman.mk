e015ef491e57dd022fbb29b45090ed65f59919a8
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-01-25 13:29:28 +0530

    Aerodactyl : Initial bringup for axion

--- lineage_Pacman.mk
+++ lineage_Pacman.mk
@@ -24,3 +24,45 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=Pacman \
     SystemDevice=Pacman \
     SystemName=Pacman
+
+# Disable EPPE
+TARGET_DISABLE_EPPE := false 
+
+# Target has blur
+TARGET_ENABLE_BLUR := true
+
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,50
+AXION_CAMERA_FRONT_INFO := 32
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := Shravan
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Mediatek_Dimensity_7200_Pro 
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= false
+
+# CPU governor support
+PERF_GOV_SUPPORTED := false
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := false
+
+# GPU
+GPU_FREQS_PATH := /sys/devices/platform/28000000.mali/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/devices/platform/28000000.mali/hint_min_freq
+
+# High Brightness Mode (HBM)
+HBM_SUPPORTED := false
+HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := false
+
+# doze fix
+# for devices with doze/sensor related issues 
+TARGET_NEEDS_DOZE_FIX := true
+
+
+TARGET_INCLUDES_LOS_PREBUILTS := false
