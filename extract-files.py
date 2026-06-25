5b21591ddd882664cb575a5acacd998eae2f5125
Saikrishna1504 <saikrishna26918@gmail.com>
2026-06-26 02:10:41 +0530

    Aerodactyl: Rename libformatter to libformatter-V33
    Change-Id: I0213eee78af45b929b1e6410f47850c1dc89667e
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: darkDolphin1 <shravanrajapure1@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -116,6 +116,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v33.so')
         .replace_needed('libui.so', 'libui-v34.so')
         .add_needed('libbase_shim.so'),
+    ('vendor/lib64/libcodec2_mtk_vdec.so', 'vendor/lib64/libcodec2_mtk_venc.so'): blob_fixup()
+        .replace_needed('libformatter.so', 'libformatter-v33.so'),
     'vendor/lib64/libcodec2_hidl@1.2-v33.so': blob_fixup()
         .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-bp2a.so')
         .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v33.so')
