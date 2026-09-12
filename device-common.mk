8833a6e8e6368e5cf4a1c0285fc818962478d9b1
darkDolphin1 <shravanrajapure1@gmail.com>
2026-09-12 14:58:56 +0000

    Revert "Aerodactyl: drop DolbyAtmos"
    This reverts commit 0853d3752ec33b760db337330cf01b3f87b5e4b6.

--- device-common.mk
+++ device-common.mk
@@ -128,6 +128,9 @@ PRODUCT_COPY_FILES += \
 # Dolby Atmos
 $(call inherit-product, hardware/dolby/dolby.mk)
 
+PRODUCT_PACKAGES += \
+    DolbyAtmos
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
